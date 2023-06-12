e4a03ca2b6af0bacb0ae12ea560f8547e036bed9
Christoph Welcz <christoph@welcz.de>
2023-06-12 11:45:11 +0200

    ♻️ extracts assertion method for JSON

--- src/test/kotlin/de/datev/samples/kotlinbeer/ApiTest.kt
+++ src/test/kotlin/de/datev/samples/kotlinbeer/ApiTest.kt
@@ -3,6 +3,7 @@ package de.datev.samples.kotlinbeer
 import io.kotest.assertions.json.shouldEqualJson
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.nulls.shouldNotBeNull
+import org.intellij.lang.annotations.Language
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest
 import org.springframework.test.web.reactive.server.WebTestClient
 import org.springframework.test.web.reactive.server.expectBody
@@ -11,24 +12,28 @@ import org.springframework.test.web.reactive.server.expectBody
 class ApiTest(private val webTestClient: WebTestClient) : DescribeSpec({
   describe("API for /beers") {
     describe("has GET /") {
+      val expected = """
+                      [
+                        {
+                          "brand": "Nestle",
+                          "name": "Wasser",
+                          "strength": 0
+                        }
+                      ]
+                    """.trimIndent()
+
       val response = webTestClient.get().uri("/beers").exchange()
 
       response.expectStatus().isOk
-      response.expectBody<String>().consumeWith {
-        it.responseBody
-          .shouldNotBeNull()
-          .shouldEqualJson(
-            """
-            [
-              {
-                "brand": "Nestle",
-                "name": "Wasser",
-                "strength": 0
-              }
-            ]
-          """.trimIndent()
-          )
-      }
+      response.shouldHaveJsonBody(expected)
     }
   }
 })
+
+private fun WebTestClient.ResponseSpec.shouldHaveJsonBody(@Language("JSON") expected: String) {
+  expectBody<String>().consumeWith {
+    it.responseBody
+      .shouldNotBeNull()
+      .shouldEqualJson(expected)
+  }
+}
