0c0b720a69247d203d3b186bc2bc28d2b790e1a7
Christoph Welcz <christoph@welcz.de>
2023-06-12 11:22:42 +0200

    ♻️ cleanup

--- src/test/kotlin/de/datev/samples/kotlinbeer/ApplicationTest.kt
+++ src/test/kotlin/de/datev/samples/kotlinbeer/ApplicationTest.kt
@@ -2,7 +2,6 @@ package de.datev.samples.kotlinbeer
 
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.nulls.shouldNotBeNull
-import io.kotest.matchers.shouldBe
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.core.env.Environment
 
